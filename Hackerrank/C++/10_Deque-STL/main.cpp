df8a61a34499c01a7ccb0634f0ea07f1ac6539a4
felixmerz <65616000+felixmerz00@users.noreply.github.com>
2022-08-13 20:38:59 +0200

    Average case improved and maximum score achieved. Even tought I removed deques...

--- Hackerrank/C++/10_Deque-STL/main.cpp
+++ Hackerrank/C++/10_Deque-STL/main.cpp
@@ -2,25 +2,35 @@
 #include <deque> 
 using namespace std;
 
+int get_max_index(int arr[], int start_i, int k){
+    int max_i;
+    for(int i = start_i; i - start_i < k; i++){
+        if(i == start_i){    // If the deque is empty, add the first element
+            max_i = i;
+        }else{  // Save the biggest element of the subarray at the front
+            if(arr[i] >= arr[max_i]){
+                max_i = i;
+            }
+        }
+    }
+    return max_i;
+}
+
 void printKMax(int arr[], int n, int k){
 	//Write your code here.
     int start_idx = 0;
-    int i;
-    int max;
-    while(n - start_idx >= k){   // Stop when the last subarrays has been reached
-        for(i = start_idx; i - start_idx < k; i++){
-            if(i == start_idx){    // If the deque is empty, add the first element
-                max = arr[i];
-            }else{  // Save the biggest element of the subarray at the front
-                if(arr[i] > max){
-                    max = arr[i];
-                }
-            }
+    int max_idx = -1;
+    while(n - start_idx >= k){  // Stop when the last subarrays has been reached
+        if(max_idx < start_idx){    // Did the max go out of range?
+            max_idx = get_max_index(arr, start_idx, k);
         }
-        cout << max << " "; // Print the maximum of the subarray
+        else if(arr[start_idx + k - 1] >= arr[max_idx]){    // Is the new element greater than the current max
+            max_idx = start_idx + k - 1;
+        }
+        printf("%d ", arr[max_idx]);    // Print the maximum of the subarray
         start_idx++;    // The next subarray should start one index higher
     }
-    cout << endl;
+    printf("\n");
 }
 
 int main(){
@@ -33,7 +43,7 @@ int main(){
     	int i;
     	int arr[n];
     	for(i=0;i<n;i++)
-      		cin >> arr[i];
+            scanf("%d", &arr[i]);
     	printKMax(arr, n, k);
     	t--;
   	}
